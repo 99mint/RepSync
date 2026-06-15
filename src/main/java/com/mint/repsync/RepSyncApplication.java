525191bc385d244a90381d032973c1a4d74dbf3c
Abhay <abhayyy2204@gmail.com>
2026-06-16 01:03:11 +0530

    Initial project setup

---
+++ src/main/java/com/mint/repsync/RepSyncApplication.java
@@ -0,0 +1,13 @@
+package com.mint.repsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepSyncApplication.class, args);
+    }
+
+}
